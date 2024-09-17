86dcf25ec4d2f9c952cfab5db421f2ac45736f12
jeancarlos.castrillon2274 <jeancarlos.castrillon2274@gmail.com>
2024-09-17 03:58:42 +0000

    create frontend proyect

---
+++ frontend/src/ui/header/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-full flex items-center md:justify-between justify-center p-4">
+      Header
+    </div>
+  );
+};
+
+export default Header;
